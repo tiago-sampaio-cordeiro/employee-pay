3aad096f2b7b356fef589a900d294930ade30825
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-02-21 23:24:16 -0300

    Money and time formats have been changed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def format_hours(decimal_hours)
+    hours = decimal_hours.to_i
+    minutes = ((decimal_hours - hours) * 60).round
+    format("%02d:%02d", hours, minutes)
+  end
 end
