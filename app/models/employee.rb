c25343855caed1261b8964beef2f154105e9c8bd
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-02-26 16:17:53 -0300

    Created phones table

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,8 @@ class Employee < ApplicationRecord
   belongs_to :user
   has_many :time_punches, dependent: :destroy
 
+  enum :contract_type, { freelancer: 0, clt: 1 }
+
+
   validates :hourly_rate, presence: true, numericality: { greater_than: 0 }
 end
