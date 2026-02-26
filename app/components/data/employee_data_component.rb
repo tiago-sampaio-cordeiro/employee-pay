29b310b9d5a8cce70739eb87a26ebbd4c33d711a
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-02-25 22:52:53 -0300

    Data employee card component created

--- app/components/data/employee_data_component.rb
+++ app/components/data/employee_data_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Data::EmployeeDataComponent < ViewComponent::Base
-  def initialize(first_name:, last_name:, role:, hourly_rate:, status:)
+  def initialize(first_name:, last_name:, email_address:, role:, hourly_rate:, status:)
     @first_name = first_name
     @last_name = last_name
+    @email_address = email_address
     @role = role
     @hourly_rate = hourly_rate
     @status = status
