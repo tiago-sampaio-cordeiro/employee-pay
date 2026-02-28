74ef7846cd702dfec5c95f17b61cf90466d81d72
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-02-27 23:35:16 -0300

    Removed unnecessary columns and foreign keys

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,6 @@
 class Employee < ApplicationRecord
   belongs_to :user
   has_many :time_punches, dependent: :destroy
-  has_many :phones, dependent: :destroy
-  has_many :pix_keys, dependent: :destroy
-  has_many :banks, dependent: :destroy
 
   enum :contract_type, { freelancer: 0, clt: 1 }
 
