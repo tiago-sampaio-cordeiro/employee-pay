8135a304db865c76b9ba6446fea8d8fd847be1f1
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-02-28 22:04:08 -0300

    Controllers and services of employee registers adjusted

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -33,6 +33,25 @@ class RegistersController < ApplicationController
       cnpj: employee_params[:cnpj],
       phone_number: employee_params[:phone_number],
       pix_key: employee_params[:pix_key],
+      rg: employee_params[:rg],
+      cpf: employee_params[:cpf],
+      mother_name: employee_params[:mother_name],
+      mother_last_name: employee_params[:mother_last_name],
+      emergency_phone_number: employee_params[:emergency_phone_number],
+      nationality: employee_params[:nationality],
+      city_born: employee_params[:city_born],
+      uf_born: employee_params[:uf_born],
+      blood_group: employee_params[:blood_group],
+      allergies: employee_params[:allergies],
+      driver_license: employee_params[:driver_license],
+      driver_license_category: employee_params[:driver_license_category],
+      driver_license_number: employee_params[:driver_license_number],
+      cep: employee_params[:cep],
+      house_number: employee_params[:house_number],
+      neighborhood: employee_params[:neighborhood],
+      city: employee_params[:city],
+      uf_live: employee_params[:uf_live],
+      reference: employee_params[:reference],
       user_id: nil
     )
 
@@ -65,6 +84,25 @@ class RegistersController < ApplicationController
       cnpj: employee_params[:cnpj],
       phone_number: employee_params[:phone_number],
       pix_key: employee_params[:pix_key],
+      rg: employee_params[:rg],
+      cpf: employee_params[:cpf],
+      mother_name: employee_params[:mother_name],
+      mother_last_name: employee_params[:mother_last_name],
+      emergency_phone_number: employee_params[:emergency_phone_number],
+      nationality: employee_params[:nationality],
+      city_born: employee_params[:city_born],
+      uf_born: employee_params[:uf_born],
+      blood_group: employee_params[:blood_group],
+      allergies: employee_params[:allergies],
+      driver_license: employee_params[:driver_license],
+      driver_license_category: employee_params[:driver_license_category],
+      driver_license_number: employee_params[:driver_license_number],
+      cep: employee_params[:cep],
+      house_number: employee_params[:house_number],
+      neighborhood: employee_params[:neighborhood],
+      city: employee_params[:city],
+      uf_live: employee_params[:uf_live],
+      reference: employee_params[:reference],
       user_id: params[:id]
     )
 
@@ -75,17 +113,20 @@ class RegistersController < ApplicationController
     end
   end
 
-
   def edit
     @user = User.find(params[:id])
   end
 
   private
+
   def employee_params
     params.permit(
-      :first_name, :last_name, :email_address, :active, :role, :password, :password_confirmation, :hourly_rate,
-      :user_id, :birth_date, :gender, :admission_date, :contract_type, :address, :salary, :pis, :ctps, :position, :cnpj,
-      :phone_number, :pix_key
+      :first_name, :last_name, :email_address, :active, :role, :password, :password_confirmation,
+      :hourly_rate, :user_id, :birth_date, :gender, :admission_date, :contract_type, :address, :salary,
+      :pis, :ctps, :position, :cnpj, :phone_number, :pix_key, :rg, :cpf, :mother_name, :mother_last_name,
+      :emergency_phone_number, :nationality, :city_born, :uf_born, :blood_group, :allergies,
+      :driver_license, :driver_license_category, :driver_license_number, :cep, :house_number,
+      :neighborhood, :city, :uf_live, :reference
     )
   end
 end
--- app/services/register/employee_records.rb
+++ app/services/register/employee_records.rb
@@ -1,8 +1,14 @@
 module Register
   class EmployeeRecords
-    def initialize(first_name:, last_name:, email_address:, active:, role:, password:, password_confirmation:,
-                   hourly_rate:, birth_date:, gender:, admission_date:, address:, contract_type:,
-                   salary:, pis:, ctps:, position:, cnpj:, phone_number:, pix_key:, user_id:)
+    def initialize(first_name:, last_name:, email_address:, active:, role:, password:,
+                   password_confirmation:, hourly_rate:, birth_date:, gender:, admission_date:,
+                   address:, contract_type:, salary:, pis:, ctps:, position:, cnpj:, phone_number:,
+                   pix_key:, rg:, cpf:, mother_name:, mother_last_name:, emergency_phone_number:,
+                   nationality:, city_born:, uf_born:, blood_group:, allergies:, driver_license:,
+                   driver_license_category:, driver_license_number:, cep:, house_number:,
+                   neighborhood:,
+                   city:, uf_live:, reference:, user_id:)
+
       @first_name = first_name
       @last_name = last_name
       @email_address = email_address
@@ -23,6 +29,25 @@ module Register
       @cnpj = cnpj
       @phone_number = phone_number
       @pix_key = pix_key
+      @rg = rg
+      @cpf = cpf
+      @mother_name = mother_name
+      @mother_last_name = mother_last_name
+      @emergency_phone_number = emergency_phone_number
+      @nationality = nationality
+      @city_born = city_born
+      @uf_born = uf_born
+      @blood_group = blood_group
+      @allergies = allergies
+      @driver_license = driver_license
+      @driver_license_category = driver_license_category
+      @driver_license_number = driver_license_number
+      @cep = cep
+      @house_number = house_number
+      @neighborhood = neighborhood
+      @city = city
+      @uf_live = uf_live
+      @reference = reference
       @user_id = user_id
     end
 
@@ -36,9 +61,13 @@ module Register
 
     private
 
-    attr_reader :user_id, :first_name, :last_name, :email_address, :active, :role, :password, :password_confirmation,
-                :hourly_rate, :contract_type, :salary, :pis, :ctps, :position, :cnpj, :gender, :birth_date,
-                :admission_date, :address, :phone_number, :pix_key
+    attr_reader :first_name, :last_name, :email_address, :active, :role, :password,
+                :password_confirmation, :hourly_rate, :contract_type, :salary, :pis, :ctps,
+                :position, :cnpj, :gender, :birth_date, :admission_date, :address, :phone_number,
+                :pix_key, :rg, :cpf, :mother_name, :mother_last_name, :emergency_phone_number,
+                :nationality, :city_born, :uf_born, :blood_group, :allergies, :driver_license,
+                :driver_license_category, :driver_license_number, :cep, :house_number,
+                :neighborhood, :city, :uf_live, :reference, :user_id
 
     def create_new_employee
       user = User.create!(
@@ -64,6 +93,25 @@ module Register
         cnpj: cnpj,
         phone_number: phone_number,
         pix_key: pix_key,
+        rg: rg,
+        cpf: cpf,
+        mother_name: mother_name,
+        mother_last_name: mother_last_name,
+        emergency_phone_number: emergency_phone_number,
+        nationality: nationality,
+        city_born: city_born,
+        uf_born: uf_born,
+        blood_group: blood_group,
+        allergies: allergies,
+        driver_license: driver_license,
+        driver_license_category: driver_license_category,
+        driver_license_number: driver_license_number,
+        cep: cep,
+        house_number: house_number,
+        neighborhood: neighborhood,
+        city: city,
+        uf_live: uf_live,
+        reference: reference,
         user: user
       )
     end
@@ -92,7 +140,26 @@ module Register
         position: position,
         cnpj: cnpj,
         phone_number: phone_number,
-        pix_key: pix_key
+        pix_key: pix_key,
+        rg: rg,
+        cpf: cpf,
+        mother_name: mother_name,
+        mother_last_name: mother_last_name,
+        emergency_phone_number: emergency_phone_number,
+        nationality: nationality,
+        city_born: city_born,
+        uf_born: uf_born,
+        blood_group: blood_group,
+        allergies: allergies,
+        driver_license: driver_license,
+        driver_license_category: driver_license_category,
+        driver_license_number: driver_license_number,
+        cep: cep,
+        house_number: house_number,
+        neighborhood: neighborhood,
+        city: city,
+        uf_live: uf_live,
+        reference: reference
       )
     end
   end
