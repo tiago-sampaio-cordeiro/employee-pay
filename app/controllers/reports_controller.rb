3ea3eba1a5ecb47d85e0c8dc13d8d967ac1286f8
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-03-01 17:00:40 -0300

    else in condition commented

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,7 +23,7 @@ class ReportsController < ApplicationController
 
   def build_report_employee(range)
     employee = Current.user.employee
-    [ build_result(employee, range) ]
+    [build_result(employee, range)]
   end
 
   def build_result(employee, range)
@@ -37,11 +37,12 @@ class ReportsController < ApplicationController
   def build_range
     if params[:start_date].present? && params[:end_date].present?
       start_date = Date.parse(params[:start_date]).beginning_of_day
-      end_date   = Date.parse(params[:end_date]).end_of_day
+      end_date = Date.parse(params[:end_date]).end_of_day
 
       start_date..end_date
     else
-      Date.current.beginning_of_month..Date.current.end_of_month
+      # Date.current.beginning_of_month..Date.current.end_of_month
+      Date.new(2026, 2, 1).beginning_of_day..Date.new(2026, 2, 28).end_of_day
     end
   end
 end
