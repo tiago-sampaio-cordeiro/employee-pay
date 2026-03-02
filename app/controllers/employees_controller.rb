217985b577c245769e2de44e11b3f621395556d8
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-03-01 22:49:49 -0300

    Add a method to restrict 2 records in less than 15 minutes.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,28 +1,36 @@
 class EmployeesController < ApplicationController
   helper_method :next_punch_kind
+
+
   def index
   end
 
   def create
-    time_clock = TimeClock::EmployeeTimeClock.new(
-      employee: Current.user.employee,
-      kind: time_clocks_params[:kind],
-      punched_at: DateTime.current).call
-    if time_clock
+    if can_register_punch?
+      time_clock = TimeClock::EmployeeTimeClock.new(
+        employee: Current.user.employee,
+        kind: time_clocks_params[:kind],
+        punched_at: DateTime.current).call
       redirect_to employees_path, notice: "Ponto registrado com sucesso!"
     else
+      flash.now[:alert] = "Aguarde 15 minutos!"
       render :index, status: :unprocessable_entity
     end
   end
 
   def next_punch_kind
-    last = Current.user.employee.time_punches.order(:punched_at).last
-    last&.clock_in? ? "clock_out" : "clock_in"
+    Current.user.employee.next_punch_kind
   end
 
   private
 
   def time_clocks_params
     params.permit(:kind)
+  end
+
+  def can_register_punch?
+    last_punch = Current.user.employee.time_punches.order(:punched_at).last
+    return true if last_punch.nil? || last_punch.clock_out?
+    last_punch.punched_at + 15.minutes <= Time.current
   end
 end
