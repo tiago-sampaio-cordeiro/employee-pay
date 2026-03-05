bf6703a6e2fcf4ca98253e35e88304ee9ccc1e78
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-03-05 20:15:57 -0300

    Fixed classes after unit tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def format_hours(decimal_hours)
     hours = decimal_hours.to_i
     minutes = ((decimal_hours - hours) * 60).round
-    format("%02d:%02d", hours, minutes)
+    seconds = ((decimal_hours - hours)) * 60
+    format("%02d:%02d:%02d", hours, minutes, seconds)
   end
 end
