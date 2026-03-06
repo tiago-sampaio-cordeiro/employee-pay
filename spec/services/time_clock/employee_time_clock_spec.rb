09dc78a5cc2706f3cc313f9c705e8463082054b0
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-03-06 01:02:30 -0300

    Created unit tests to hoursCalculator service

---
+++ spec/services/time_clock/employee_time_clock_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Employee, type: :service do
+  let(:employee) {create :employee}
+
+  describe "punch" do
+    it "created correctly" do
+
+    end
+
+  end
+end
