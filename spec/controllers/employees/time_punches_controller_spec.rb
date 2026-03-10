de7cc78ecaf0b41c9e432d35470f4a7fbe62661c
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-03-10 11:39:28 -0300

    Created unit tests to SessionsController controller

--- spec/controllers/employees/time_punches_controller_spec.rb
+++ spec/controllers/employees/time_punches_controller_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Employees::TimePunchesController, type: :controller do
     context "when user is admin" do
       it "redirects to admin root" do
         sign_in admin
-        post :create, params: {kind: :clock_in}
+        post :create, params: { kind: :clock_in }
         expect(response).to redirect_to(admin_root_path)
       end
     end
@@ -41,7 +41,7 @@ RSpec.describe Employees::TimePunchesController, type: :controller do
     context "when user is employee" do
       it "creates time_punch and returns success with notice" do
         sign_in employee_user
-        post :create, params: {kind: :clock_in}
+        post :create, params: { kind: :clock_in }
         expect(response).to redirect_to(employees_time_punches_path)
         expect(flash[:notice]).to eq("Ponto registrado com sucesso!")
       end
@@ -51,7 +51,7 @@ RSpec.describe Employees::TimePunchesController, type: :controller do
       it "redirect to time punches index with notice" do
         create(:time_punch, employee: employee_user.employee, kind: :clock_in, punched_at: Time.current)
         sign_in employee_user
-        post :create, params: {kind: :clock_out}
+        post :create, params: { kind: :clock_out }
         expect(response).not_to redirect_to(employees_time_punches_path)
         expect(flash[:alert]).to eq("Aguarde 15 minutos!")
       end
